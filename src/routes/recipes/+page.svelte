8fd2d8b14eb70a2929281f930dd6e09339cec469
MarijaUgljenovic <marija.ugljenovic99@gmail.com>
2025-03-27 13:37:57 +0100

    Recipes

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -1,22 +1,48 @@
-<script>
+<script lang="ts">
     export let data; 
     console.log(data); 
 
     const { recipes } = data;
+
+    let selectedRecipe: { 
+        name: string; 
+        ingredients: string; 
+        instructions: string;
+    } | null = null;
+
+    function selectRecipe(recipe: { name: string; ingredients: string; instructions: string }) {
+        selectedRecipe = recipe;
+    }
+
+    function goBack() {
+        selectedRecipe = null;
+    }
 </script>
 
 <h1>Menu</h1>
+
+{#if selectedRecipe}
+    <div class="recepie-details">
+        <button on:click={goBack} class="back-btn">Back</button>
+        <h2>{selectedRecipe.name}</h2>
+        <p><strong>Ingredients</strong>{selectedRecipe.ingredients}</p>
+        <p><strong>Instructions:</strong> {selectedRecipe.instructions}</p>
+    </div>
+{:else}
 <div class="menu-container">
     {#each recipes as recipes}
-    <div class="menu-item">
-        <h2>{recipes.name}</h2>
-        <p>
-            <strong>Ingredients:</strong>
-            {recipes.name}
-        </p>
-    </div>
-{/each}
+        <div class="menu-item" on:click={() => selectRecipe(recipes)}>
+            <h2>{recipes.name}</h2>
+            <p>
+                <strong>Ingredients:</strong>
+                {recipes.ingredients}
+            </p>
+        </div>
+    {/each}
 </div>
+{/if}
+
+
 
 <style>
     h1{
@@ -53,4 +79,34 @@
 .menu-item p{
     color: #555;
 }
+
+.recipe-details {
+        background: #fff;
+        padding: 2rem;
+        border-radius: 12px;
+        box-shadow: 2px 4px 15px rgba(0, 0, 0, 0.1);
+        text-align: left;
+        max-width: 600px;
+        margin: auto;
+    }
+
+    .recipe-details h2 {
+        color: #d35400;
+        margin-bottom: 1rem;
+    }
+
+    .back-btn {
+        background: #d35400;
+        color: white;
+        border: none;
+        padding: 0.5rem 1rem;
+        border-radius: 8px;
+        cursor: pointer;
+        margin-bottom: 1rem;
+        font-size: 1rem;
+    }
+
+    .back-btn:hover {
+        background: #e67e22;
+    }
 </style>
