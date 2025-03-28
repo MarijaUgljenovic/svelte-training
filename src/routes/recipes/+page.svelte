2b35cd3529acef3f5eb2823d449f4aa06c59580c
MarijaUgljenovic <marija.ugljenovic99@gmail.com>
2025-03-28 16:33:37 +0100

    Recipes

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -1,23 +1,43 @@
 <script lang="ts">
+    import {onMount} from "svelte";
+    import Sortable from "sortablejs";
+
     export let data; 
     console.log(data); 
     const { recipes } = data;
+
+    let recipeList;
+
+    onMount(() => {
+
+        new Sortable(recipeList, {
+            animation: 150,
+            ghostClass: "dragging",
+            onEnd: (evt) =>{
+                const {oldIndex, newIndex} = evt;
+                if (oldIndex !== newIndex){
+                    const movedItem = recipes.splice(oldIndex, 1)[0];
+                    recipes.splice(newIndex, 0, movedItem);
+                }
+            }
+        })
+    });
 
 </script>
 
 <h1>Menu</h1>
 
 
-<div class="menu-container">
+<ul bind:this={recipeList} class="menu-container">
     {#each recipes as recipe}
-        <a class="menu-item" href={`/recipes/${recipe.name.toLowerCase().replace(/\s+/g, '-')}`}>
-            <h2>{recipe.name}</h2>
-            <p>
-                <strong>Ingredients:</strong> {recipe.ingredients}
-            </p>
-        </a>
+        <li class="menu-item">
+            <a href={`/recipes/${recipe.name.toLowerCase().replace(/\s+/g, '-')}`}>
+                <h2>{recipe.name}</h2>
+                <p><strong>Ingredients:</strong> {recipe.ingredients}</p>
+            </a>
+        </li>
     {/each}
-</div>
+</ul>
 
 <style>
     h1{
@@ -27,33 +47,39 @@
     }
 
     .menu-container {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-        gap: 1.5rem;
-        padding:1rem
+        list-style: none;
+        padding: 0;
+        width: 80%;
+        margin: auto;
     }
 
     .menu-item{
         background: #f8f9fa;
-        border-radius:12px;
+        border-radius: 12px;
         padding: 1.5rem;
+        margin: 0.5rem 0;
         box-shadow: 2px 4px 10px rgba(0, 0, 0, 0.1);
-        text-align: center;
-        text-decoration: none;
-        transition:transform 0.3s ease-in-out;
+        cursor: grab;
+        transition: transform 0.3s ease-in-out;
     }
 
-    .menu-item:hover{
-        transform: scale(1.05);
+    .menu-item a {
+        text-decoration: none; /* Uklanja podvučene linkove */
+        color: inherit;
+        display: block;
     }
 
-    .menu-item h2{
+    .menu-item h2 {
         margin-bottom: 0.5rem;
         color: #d35400;
     }
 
-    .menu-item p{
+    .menu-item p {
         color: #555;
+    }
+
+    .dragging {
+        opacity: 0.5;
     }
 
     
